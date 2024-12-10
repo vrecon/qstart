3872b556cfe5dc50ee1111b909939f5076094e98
Barry Vredevoort <bvredevoort@apro.nl>
2024-12-10 14:59:44 +0100

    test

--- src/main/java/com/silascandiolli/GreetingResource.java
+++ src/main/java/com/silascandiolli/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST 1";
+        return "Hello from Quarkus REST 1 test test";
     }
 }
